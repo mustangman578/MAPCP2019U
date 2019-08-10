cfee3b7a99ff74d7a79d90e5d1626c51075456cc
Andrew Myers <andrew.myers@mavs.uta.edu>
2019-08-10 13:40:35 -0500

    Changes to be committed: 	modified:   monty_carlo_game.py  Changes not staged for commit: 	deleted:    ../../exams/final/src/load_mat.py  Untracked files: 	../../exams/final/results/ 	../../exams/final/src/get_subplots.py

--- homework/4/monty_carlo_game.py
+++ homework/4/monty_carlo_game.py
@@ -1,42 +1,38 @@
 import random
-def getWins_choose():
-    win = False
+from matplotlib import pyplot as plt
+import numpy as np
 
-    choice = random.randint(1,3)
+def get_chance_of_win_switch():
     #prize is 1
-    
+    choice = random.randint(1,3)
     if choice == 2 or choice == 3:
-        win = True
-    return win
-        
-    
-def getWins_stay():
-    win = False
+        return 1.0
+    else:
+        return 0.0
+
+def get_chance_of_win_stay():
     #prize is 1
     choice = random.randint(1,3)
-    
     if choice == 1:
-        win = True
-    return win
+        return 1.0
+    else:
+        return 0.0
 
+nsim = 100000
+avg_chance_of_win_stay = np.zeros(nsim)
+avg_chance_of_win_switch = np.zeros(nsim)
+experiment_number = [i for i in range(0,nsim)]
 
-    
-    
-    
-    
-n = 100000    
-avg_choice = 0
-avg_stay = 0
+avg_chance_of_win_stay[0] = get_chance_of_win_stay()
+avg_chance_of_win_switch[0] = get_chance_of_win_switch()
+for i in experiment_number[1:]:
+    avg_chance_of_win_stay[i] = (i*avg_chance_of_win_stay[i-1] + get_chance_of_win_stay()) / (i+1)
+    avg_chance_of_win_switch[i] = (i*avg_chance_of_win_switch[i-1] + get_chance_of_win_switch()) / (i+1)
 
+print( "  average chance of winning by staying: {}".format(avg_chance_of_win_stay[-1]) )
+print( "average chance of winning by switching: {}".format(avg_chance_of_win_switch[-1]) )
 
-
-for i in range(n):
-    Win_choose = getWins_choose()
-    Win_stay = getWins_stay()
-    if Win_choose == True:
-        avg_choice += 1/n
-    if Win_stay == True:
-        avg_stay += 1/n
-        
-print(avg_choice)
-print(avg_stay)
+plt.semilogx(experiment_number,avg_chance_of_win_stay)
+plt.semilogx(experiment_number,avg_chance_of_win_switch)
+#plt.xscale('log')
+plt.show()
