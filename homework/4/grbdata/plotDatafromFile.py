c1d1e17781bc581dfe1124df42087903ac1118c3
Andrew Myers <andrew.myers@mavs.uta.edu>
2019-08-14 14:01:56 -0500

    modified:   Untitled.ipynb 	modified:   plotDatafromFile.py

--- homework/4/grbdata/plotDatafromFile.py
+++ homework/4/grbdata/plotDatafromFile.py
@@ -1,12 +1,19 @@
 import os
+import matplotlib.pyplot as plt
 
+x_list = []
+y_list = []
 filelist = os.listdir()
-
 for i in filelist:
     if i.endswith('.txt'):
         with open(i,'r',encoding='ascii') as y:
-            try:
-                for line in y:
-                    data = line.split('\\n')
-            except:
-                pass
+            for line in y:
+                data = line.split('\\n')
+                for i in data:
+                    y_value = i[0:5]
+                    x_value = i[7:13]
+                    x_list.append(x_value)
+                    y_list.append(y_value)
+
+plt.scatter(x_list,y_list)
+plt.show()
